b20fc3246106968b3c2d284029101d3b1151142c
NEERAJ JHA <neeraj.nj.260701@gmail.com>
2022-08-12 22:27:23 +0530

    context auth

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import './App.css';
 import SignUp from './components/SignUp';
 import Login from './components/Login'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { AuthProvider } from './Context/AuthContext';
 
 function App() {
   return (
     <div className="App">
 
       <BrowserRouter>
+      <AuthProvider>
       <Routes>
         <Route path ='/' element ={<SignUp/>}/>
         <Route path = '/login' element={<Login/>} />
       </Routes>
+      </AuthProvider>
       
       </BrowserRouter>
      
